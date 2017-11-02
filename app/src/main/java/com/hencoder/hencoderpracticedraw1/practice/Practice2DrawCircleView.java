a641d0d21c0c663b63c90909606f078a8ac753cb
邱三鑫 <qiusanxin@qiusanxindeMacBook-Pro.local>
2017-11-02 21:20:36 +0800

    提交前四个练习题

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -2,11 +2,15 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice2DrawCircleView extends View {
+
+    private Paint mPaint;
 
     public Practice2DrawCircleView(Context context) {
         super(context);
@@ -14,6 +18,7 @@ public class Practice2DrawCircleView extends View {
 
     public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mPaint=new Paint();
     }
 
     public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -26,5 +31,30 @@ public class Practice2DrawCircleView extends View {
 
 //        练习内容：使用 canvas.drawCircle() 方法画圆
 //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+
+//        1.实心圆
+
+        mPaint.setColor(Color.BLACK);
+        canvas.drawCircle(300, 200, 150, mPaint);
+
+//        2.空心圆
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(3);
+        canvas.drawCircle(700, 200, 150, mPaint);
+
+//        3.蓝色实心圆
+        mPaint.reset();
+        mPaint.setColor(Color.BLUE);
+        canvas.drawCircle(300, 600, 150, mPaint);
+
+//        4.线款为20的圆环
+        mPaint.reset();
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(20);
+        mPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawCircle(700, 600, 150, mPaint);
+
+
+
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
@@ -2,11 +2,16 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice3DrawRectView extends View {
+
+    private Paint mPaint;
 
     public Practice3DrawRectView(Context context) {
         super(context);
@@ -14,6 +19,7 @@ public class Practice3DrawRectView extends View {
 
     public Practice3DrawRectView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mPaint=new Paint();
     }
 
     public Practice3DrawRectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -25,5 +31,9 @@ public class Practice3DrawRectView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawRect() 方法画矩形
+
+        mPaint.setColor(Color.BLACK);
+        canvas.drawRect(400, 200, 800, 600, mPaint);
+
     }
 }
